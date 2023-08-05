853e594080e1f466965c60f8f6cbc1582a96d3c9
Iuridev <iurigames23@gmail.com>
2023-08-04 22:50:23 -0300

    Formatação inicial

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -6,3 +6,20 @@ Escolha uma opção:
 0 - Sair
 
 """
+
+saldo = 0
+LIMITE = 500
+extrato = ""
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == 1:
+        print("Deposito")
+    elif opcao == 2:
+        print("Saque")
+    elif opcao == 3:
+        print("Extrato")
+    else:
+        print("Opção inexistente! Selecione outra opção.")
